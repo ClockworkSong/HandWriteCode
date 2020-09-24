f12f4f960174d9e0965d35f7e3435290b92fd184
song_xd <244549970@qq.com>
2020-09-24 21:56:47 +0800

    add server.cpp

--- server/server.cpp
+++ server/server.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 #include <cstdio> // perror()
-#include <ctime> // ctime()
 #include <unistd.h> // close()
 
 #include <sys/types.h>
@@ -9,9 +8,32 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define AXIS_COUNT 8
+#define ANALOG_IN_NUM 3
+#define ANALOG_OUT_NUM 5
+
 static int PORT = 8056;
 
 uint8_t buf[8] = {0x1, 0x2, 0x3, 0x4, 0x11, 0x22, 0x33, 0xff};
+
+typedef struct robot_data {
+    uint32_t messageSize;                   // 数据包长度 4Byte
+    uint64_t timestamp;                     // 时间戳  8Byte
+    uint8_t autorunCycelMode;               // 循环模式 1Byte
+    double machinePos[AXIS_COUNT];          // 关节角度，单位：度 64Byte
+    double machinePose[6];                  // 直角坐标，X,Y,Z单位：毫米 Rx,Ry,Rz单位：弧度 48Byte
+    double machineUserPose[6];              // 用户坐标，X,Y,Z单位：毫米 Rx,Ry,Rz单位：弧度 48Byte
+    double torque[AXIS_COUNT];              // 关节额定力矩百分比，单位：百分比 64Byte
+    int32_t robotState;                     // 机器人状态，0代表停止，1代表暂停，2代表急停，3代表运行，4代表报警 4Byte
+    int32_t servoReady;                     // 抱闸状态，0代表未打开，1代表已打开 4Byte
+    int32_t canMotorRun;                    // 同步状态，0代表未同步，1代表同步 4Byte
+    int32_t motorSpeed[AXIS_COUNT];         // 电机速度，单位：转/分 32Byte
+    int32_t robotMode;                      // 机器人模式，0代表示教模式，1代表自动模式，2代表远程模式 4Byte
+    double analogIoInput[ANALOG_IN_NUM];    // 模拟量输入口数据，单位：伏特 24Byte
+    double analogIoOutput[ANALOG_OUT_NUM];  // 模拟量输出口数据，单位：伏特 40Byte
+    uint64_t digitalIoInput;                // 数字量输入数据  8Byte
+    uint64_t digitalIoOutput;               // 数字量输出数据  8Byte
+}robot_data_t;
 
 int main()
 {
@@ -40,7 +62,7 @@ int main()
         perror("listen error:");
         return -1;
     }
-    printf("size of buf = %d\n", sizeof(buf));
+    printf("size of robot_data = %d\n", sizeof(robot_data_t));
     // 客户端信息
     struct sockaddr_in client_addr{};
     socklen_t client_addr_len = sizeof(sockaddr_in);

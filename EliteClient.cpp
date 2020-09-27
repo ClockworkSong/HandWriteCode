b75e24c914095f2e9cc9e486a7aab2672163a883
song_xd <244549970@qq.com>
2020-09-28 00:38:19 +0800

    add big2little_endian func

--- EliteClient.cpp
+++ EliteClient.cpp
@@ -37,6 +37,34 @@ typedef struct robot_data {
 }robot_data_t;
 #pragma pack(pop)
 
+//大端转小端
+
+short big2little_16(short be)
+{
+    return ((be >> 8) &0xff ) // move byte 1 to byte 0
+           | ((be << 8) & 0xff00);// byte 0 to byte 1
+}
+
+int big2little_32(int be)
+{
+    return ((be >> 24) &0xff ) // move byte 3 to byte 0
+           | ((be >> 8) & 0xFF00) // move byte 2 to byte 1
+           | ((be << 8) & 0xFF0000)// move byte 1 to byte 2
+           | ((be << 24) & 0xff000000);// byte 0 to byte 3
+}
+
+//double big2little_64(double be)
+//{
+//    return ((be >> 56) & 0xff ) // move byte 7 to byte 0                    0x12
+//           | ((be >> 40) & 0xFF00)// move byte 6 to byte 1                  0x34
+//           | ((be >> 24) & 0xFF0000)// move byte 5 to byte 2                0x56
+//           | ((be >> 8) & 0xFF000000)// move byte 4 to byte 3               0x78
+//           | ((be << 8) & 0xFF00000000)// move byte 3 to byte 4             0x76
+//           | ((be << 24) & 0xFF0000000000)// move byte 2 to byte 5          0x54
+//           | ((be << 40) & 0xFF000000000000)// move byte 1 to byte 6        0x32
+//           | ((be << 56) & 0xFF00000000000000);// move byte 0 to byte 7     0x10
+//}
+
 int main()
 {
     //1. 调用socket函数创建套接字
@@ -51,7 +79,7 @@ int main()
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_addr.s_addr = inet_addr("10.211.55.5");
     int socket_connect = connect(socket_fd, (struct sockaddr *) &server_addr, sizeof(sockaddr_in));
     if(socket_connect == -1) {
         perror("bind error:");
@@ -64,9 +92,18 @@ int main()
     if (ret == -1) {
         perror("recv error:");
     }
+
+    short x = 0x1234;
+    printf("before x= %x, after x=%x\n", x, big2little_16(x));
+    int y = 0x12345678;
+    printf("before y= %x, after y=%x\n", y, big2little_32(y));
+
+//    double z = 0x1234567876543210;
+//    printf("before z= %x, after z=%x\n", y, big2little_64(z));
+
     printf("recv byte = %d\n", ret);
-    printf("messageSize = %d\n", rcv_buf.messageSize);
-    printf("timestamp = %lu\n", rcv_buf.timestamp);
+    printf("messageSize = %d\n", big2little_32(rcv_buf.messageSize));
+    printf("timestamp = %llu\n", rcv_buf.timestamp);
     printf("autorunCycelMode = %d\n", rcv_buf.autorunCycelMode);
     printf("robotState = %d\n", rcv_buf.robotState);
     printf("servoReady = %d\n", rcv_buf.servoReady);
@@ -79,3 +116,31 @@ int main()
 
     return 0;
 }
+
+
+/*
+ *
+0x1234
+0001 0010 0011 0100
+>> 8 = 0001 0010 = 0x12
+<< 8 = 0011 0100 0000 0000 =0x3400
+
+0x12345678
+0001 0010 0011 0100 0101 0110 0111 1000
+>> 24 = 0001 0010 = 0x12
+>> 8  = 0001 0010 0011 0100 0101 0110 = 0x123456
+<< 8  = 0011 0100 0101 0110 0111 1000 0000 0000 = 0x34567800
+<< 24 = 0111 1000 0000 0000 0000 0000 0000 0000 = 0x78000000
+
+0x12345678 76543210
+0001 0010 0011 0100 0101 0110 0111 1000 0111 0110 0101 0100 0011 0010 0001 0000
+>> 56 = 0001 0010 = 0x12
+>> 40 = 0001 0010 0011 0100 0101 0110 = 0x123456
+>> 24 = 0001 0010 0011 0100 0101 0110 0111 1000 0111 0110 = 0x1234567876
+>> 8  = 0001 0010 0011 0100 0101 0110 0111 1000 0111 0110 0101 0100 0011 0010 = 0x12345678765432
+<< 8  = 0011 0100 0101 0110 0111 1000 0111 0110 0101 0100 0011 0010 0001 0000 0000 0000 = 0x34567876 54321000
+<< 24 = 0111 1000 0111 0110 0101 0100 0011 0010 0001 0000 0000 0000 0000 0000 0000 0000 = 0x78765432 10000000
+<< 40 = 0101 0100 0011 0010 0001 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 = 0x54321000 00000000
+<< 56 = 0001 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 = 0x10000000 00000000
+ *
+ */

ff282d3b7aa85e281e7e74186d9e047c5a200644
songxiaodong <songxiaodong@elibot.cn>
2021-11-20 14:36:38 +0800

    update 08

--- test/stringTest.cpp
+++ test/stringTest.cpp
@@ -7,7 +7,7 @@ int main()
 	const char* name = "songxiaodong";
 
 	printf("name = %s, mylen = %lu\n", name, mystrlen(name));
-	printf("Hello world 07\n");
+	printf("Hello world 08\n");
 
 	return 0;
 }

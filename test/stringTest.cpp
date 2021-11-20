b8233e90cd05f47c08037c449f52a858e018a819
“songxiaodong” <244549970@qq.com>
2021-11-20 14:07:25 +0800

    update 04

--- test/stringTest.cpp
+++ test/stringTest.cpp
@@ -7,7 +7,7 @@ int main()
 	const char* name = "songxiaodong";
 
 	printf("name = %s, mylen = %lu\n", name, mystrlen(name));
-	printf("Hello world 03\n");
+	printf("Hello world 04\n");
 
 	return 0;
 }

52f265d6212ec66489965cdb632d089afb788c16
songxiaodong <244549970@qq.com>
2021-11-21 13:28:55 +0800

    update 19

--- test/stringTest.cpp
+++ test/stringTest.cpp
@@ -7,7 +7,7 @@ int main()
 	const char* name = "songxiaodong";
 
 	printf("name = %s, mylen = %lu\n", name, mystrlen(name));
-	printf("Hello world 18\n");
+	printf("Hello world 19\n");
 
 	return 0;
 }
